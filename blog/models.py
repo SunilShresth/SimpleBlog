3211ad58150789a91b74cb296f804a5aed4f4016
Sunil Shrestha <imsunil266@gmail.com>
2019-02-07 13:51:37 +0545

    added models and views!

---
+++ blog/models.py
@@ -0,0 +1,56 @@
+from django.db import models
+from datetime import date
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class BlogAuthor(models.Model):
+    "Model representing author of a blog"
+    name = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=400, help_text="Update your bio here.")
+
+    class Meta:
+        ordering = ['name', 'bio']
+
+    def get_absolute_url(self):
+        return reverse('blogs-by-author')
+
+    def __str__(self):
+        return self.name
+
+
+class Blog(models.Model):
+    "Model representing a blog post"
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    author = models.ForeignKey(BlogAuthor, on_delete=models.SET_NULL, null=True)
+    post_date = models.DateField(default=date.today)
+
+    class Meta:
+        ordering = ['-post_date']
+
+    def get_absolute_url(self):
+        """
+        Returns the url to access a particular blog instance.
+        """
+        return reverse('blog-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+
+class BlogComment(models.Model):
+    "Model for representing comments of a blog post"
+    description = models.TextField(max_length=1000, help_text="Enter your comment here.")
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+      # Foreign Key used because BlogComment can only have one author/User, but users can have multiple comments
+    post_date = models.DateTimeField(auto_now_add=True)
+    blog= models.ForeignKey(Blog, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['post_date']
+
+    def __str__(self):
+        return self.description
---
+++ blog/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+
+from django.views import generic
+from .models import Blog, BlogAuthor, BlogComment
+from django.contrib.auth.models import User
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class BlogListView(generic.ListView):
+    model = Blog
+    paginate_by = 5
+    template_name = 'blogs/blog.html'
+
+class BlogDetailView(generic.DetailView):
+    """
+    Generic class-based detail view for a blog.
+    """
+    model = Blog
+
+
+class BloggerListView(generic.ListView):
+    model = BlogAuthor
+    paginate_by = 5
+
+
+class BlogsByAuthorListView(generic.ListView):
+    """
+    View for a list of blogs posted by a particular blogger
+    """
+    model = Blog
+    paginate_by = 5
+   # template_name = 
+
+    def get_queryset(self):
+        """
+        Return list of Blog objects created by BlogAuthor
+        """
+        id = self.kwargs['pk']
+        by_author = get_object_or_404(BlogAuthor, pk=id)
+        return Blog.objects.filter(author=by_author)
+
+    def get_context_data(self, **kwargs):
+        """
+        Add BlogAuthor to context so they can be displayed in the template
+        """
+        context = super(BlogsByAuthorListView, self).get_context_data(**kwargs)
+        context['blogger'] = get_object_or_404(BlogAuthor, pk=self.kwargs['pk'])
+        return context
