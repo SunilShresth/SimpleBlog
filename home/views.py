57d5ffee02be5cb53344ec19cd4a0b87227d43f3
Sunil Shrestha <imsunil266@gmail.com>
2019-02-17 14:09:08 +0545

    adjusted template inheritance

--- home/views.py
+++ home/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
 
 # do this later after creating models for home page ie models for training courses
 #class IndexView(generic)
 
 def index(request):
     return render(request, 'home/index.html')
+    
+
+
+def login_user(request):
+    return render(request, 'authenticate/login.html', {})
