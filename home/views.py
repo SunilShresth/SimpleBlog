f79b4b09f82e8940151f10e041ff9244a1f45837
Sunil Shrestha <imsunil266@gmail.com>
2019-02-19 18:02:27 +0545

    Added sign up form

--- home/views.py
+++ home/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from .form import SignUpForm
 
 # do this later after creating models for home page ie models for training courses
 #class IndexView(generic)
@@ -19,6 +22,32 @@ def login_user(request):
             login(request, user)
             return redirect('index')
         else:
+            messages.error(request, ('Error in logging in.. please try again!'))
             return redirect('login')
     else:
         return render(request, 'authenticate/login.html', {})
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been successfully logged out!'))
+    return redirect('login')
+
+def signup_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # username = form.cleaned_data['username']
+            # password = form.cleaned_data['password1']
+            # user = authenticate(username=username, password = password)
+            # login(request, user)
+            messages.success(request, ('You have successfully Signed up!!'))
+            return redirect('login')
+        else:
+            messages.error(request, ('Your form is not validated! Try again!!'))
+            return redirect('signup')
+
+    else:
+        form = SignUpForm()
+    context = {'form':form}
+    return render(request, 'authenticate/signup.html', context)
